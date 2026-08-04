e15dbf2ba0117557d5f5741f176128bd9c8f1a9c
Pedro Heinrich <pedroheinrich91@gmail.com>
2026-08-03 22:55:41 -0300

    feat: implementa funções que disparam as funções feitas no reducer

--- my-app/Context/ChatContext.tsx
+++ my-app/Context/ChatContext.tsx
@@ -1,14 +1,39 @@
 import { ChatReducer } from "@/reducer/ChatReducer";
 import { ChatContextType } from "@/types/ChatContextType"
-import { createContext, useReducer } from "react"
+import { createContext, useReducer, useState } from "react"
 
 const ChatContext = createContext<ChatContextType | null>(null)
 
 function ChatProvider() {
-    const [ChatList, dispatch] = useReducer(ChatReducer, []);
+    const [messages, dispatch] = useReducer(ChatReducer, []);
+    const [user, setUser] = useState('') // guarda o nome do usuario
+
+    // PROXIMO PASSO É VERIFICAR COMO VAI SABER QUE AQUELE USUARIO PERTENCE A TAL CONVERRSA SALVA
+
+    // função para disparar a função de dentro do reducer para adicionar mensagem a lista, e quem mandou
+    function addMessage(text: string, sender: 'bot'| 'user') {
+        dispatch({
+            type: 'addChat',
+            payload: {
+                sender: sender,
+                text: text
+            }
+        })
+    }
+    // função para disparar a função de dentro do reducer para remover mensagem da lista
+    function addRemove(id: string) {
+        dispatch({
+            type: 'removeChat',
+            payload: {
+                id: id
+            }
+        })
+    }
+
+
 
     return(
-        <ChatContext.Provider value={null}>
+        <ChatContext.Provider value={{user,}}>
 
         </ChatContext.Provider>
     )
